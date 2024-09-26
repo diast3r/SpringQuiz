04eacba0e9699622e4333584873135a7932df484
양준호 <didwnsgugh@gmail.com>
2024-09-26 20:47:56 +0900

    초기환경 세팅

---
+++ src/main/java/com/quiz/test/TestController.java
@@ -0,0 +1,16 @@
+package com.quiz.test;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		return "hello world";
+	}
+}
