371ded50bc4404212a448f0b41a61401d6012a50
양준호 <didwnsgugh@gmail.com>
2024-10-25 21:44:39 +0900

    Day18 spring_quiz 학원에서

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -1,5 +1,6 @@
 package com.quiz.lesson07;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,21 +33,31 @@ public class Lesson07Quiz02RestController {
 	
 	@GetMapping("/3")
 	public List<RecruitEntity> quiz02_3() {
+		// 웹 back-end 개발자이면서 정규직인 공고 조회
 		return recruitRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
 	}
 	
 	@GetMapping("/4")
 	public List<RecruitEntity> quiz02_4() {
+		// 정규직이거나 연봉이 9000 이상인 공고
+		// (Type)And(SalaryGreaterThanEqual)
 		return recruitRepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
 	}
 	
 	@GetMapping("/5")
 	public List<RecruitEntity> quiz02_5() {
+		// 계약직 목록을 연봉 기준으로 내림차순 정렬해서 3개만 조회
 		return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
 	}
 	
 	@GetMapping("/6")
 	public List<RecruitEntity> quiz02_6() {
+		// 성남시 분당구가 지역인 연봉 7000 이상 8500 이하인 공고를 조회
 		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+	}
+	
+	@GetMapping("/7")
+	public List<RecruitEntity> quiz02_7() {
+		return recruitRepository.findCondition("2024-4-10", 8100, "정규직");
 	}
 }
--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -1,8 +1,11 @@
 package com.quiz.lesson07.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.quiz.lesson07.entity.RecruitEntity;
 
@@ -16,5 +19,15 @@ public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>
 	
 	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
 	
-	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int salaryDown, int salaryUp);
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String region, int salaryStart, int salaryEnd);
+	
+	@Query(value = "select * from recruit "
+			+ "where deadline >= :deadline and "
+			+ "salary >= :salary and "
+			+ "type = :type "
+			+ "order by salary desc", nativeQuery = true)
+	public List<RecruitEntity> findCondition(
+			@Param("deadline") String deadline, 
+			@Param("salary") int salary, 
+			@Param("type") String type);
 }
